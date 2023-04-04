177488b4736491c60072e20ae8877cb94c45ddc1
Vjacheslav Borisov <slavb@iconicompany.com>
2023-04-04 21:17:01 +0400

    LDAPResource to function

--- src/LDAPResource.js
+++ src/LDAPResource.js
@@ -1,31 +1,29 @@
-export default class LDAPResource {
-  constructor(ldapClient, base) {
-    this.ldapClient = ldapClient;
-    this.base = base;
-    this.lookupCount = 0;
-  }
-
-  async lookup(name, base) {
-    base = base || this.base;
-    const options = {
-      filter: `(&(objectClass=applicationProcess)(cn=${name}))`,
-      scope: 'sub',
-      attributes: ['labeledURI']
-    };
-    let entries = [];
-    try {
-      entries = await this.ldapClient.search(base, options);
-    } catch (ex) {
-      if (ex.name !== 'NoSuchObjectError') {
-        throw new Error('LDAP lookup failed ' + ex);
-      }
-    }
-    //console.log('entries',entries);
-    let result = null;
-    if (entries.length > 0 && entries[0].labeledURI) {
-      result = entries[0].labeledURI;
-    }
-    this.lookupCount++;
-    return result;
-  }
+export default function LDAPResource(ldapClient, base) {
+  this.ldapClient = ldapClient;
+  this.base = base;
+  this.lookupCount = 0;
 }
+
+LDAPResource.prototype.lookup = async function (name, base) {
+  base = base || this.base;
+  const options = {
+    filter: `(&(objectClass=applicationProcess)(cn=${name}))`,
+    scope: 'sub',
+    attributes: ['labeledURI']
+  };
+  let entries = [];
+  try {
+    entries = await this.ldapClient.search(base, options);
+  } catch (ex) {
+    if (ex.name !== 'NoSuchObjectError') {
+      throw new Error('LDAP lookup failed ' + ex);
+    }
+  }
+  //console.log('entries',entries);
+  let result = null;
+  if (entries.length > 0 && entries[0].labeledURI) {
+    result = entries[0].labeledURI;
+  }
+  this.lookupCount++;
+  return result;
+};
