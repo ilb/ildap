47d36b975b99668ac6a9dbc2fe2301acbd4d4be6
Vjacheslav Borisov <slavb@iconicompany.com>
2023-04-04 13:25:22 +0400

    fix tests with docker ldap

---
+++ test/openldap/openldap.sh
@@ -0,0 +1,17 @@
+#docker run --detach --rm --name openldap \
+#  --env LDAP_ADMIN_USERNAME=admin \
+#  --env LDAP_ADMIN_PASSWORD=adminpassword \
+#  --env LDAP_USERS=customuser \
+#  --env LDAP_PASSWORDS=custompassword \
+#  -p 1389:1389 \
+#  bitnami/openldap:latest
+
+
+docker run  \
+    --env LDAP_ORGANISATION="Iconicompany" \
+    --env LDAP_DOMAIN="iconicompany.com" \
+    --env LDAP_ADMIN_PASSWORD="StrongAdminPassw0rd" \
+    --env LDAP_BASE_DN="dc=iconicompany,dc=com" \
+    --volume ./ldif:/container/service/slapd/assets/config/bootstrap/ldif/custom \
+   -p 1389:389 \
+   osixia/openldap:latest --copy-service
